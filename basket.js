22ce5d52749a92fe18df88ef25165cb6241ab5a1
volkan karaali <volkankaraali1@gmail.com>
2021-10-22 20:17:51 +0300

    last

--- basket.js
+++ basket.js
@@ -1,40 +1,32 @@
-let basket=[];
+let basket = [];
 
-function addProduct(id,year){
-    // fetch("./products.json").then((response)=>response.json()).then((data)=> data.map(e=>{
-    //     console.log(e);
-    //     if (e.id==id) {
-    //         console.log(e);
-    //         basket.push(e)
-    //         console.log(e.price);
-    //         basketList()
-    //     }
-    // }))
+function addProduct(id) {
 
-    console.log("add: "+year);
-    fetch("./products.json").then((response)=>response.json()).then((data)=> {
-        let product=data.find(p => p.id==id)
-        console.log(product);
+    fetch("./products.json").then((response) => response.json()).then((data) => {
+        let product = data.find(p => p.id == id)
         basket.push(product)
         basketList()
     })
-    
+
 }
 
-function basketList(){
-    
-    //console.log(basket);
-    document.getElementById("litest").innerHTML="";
-    const counts={}
-    basket.forEach((product,index) => {
-       counts[product.id]=(counts[product.id] || 0) + 1;
-    });
-    Object.entries(counts).forEach(([key,val]) => {
-        //console.log(key,val);
+function basketList() {
+  
+    basket.length!==0 ? document.getElementById("emptyAlert").style.visibility="hidden" : "visible"
+    document.getElementById("product").innerHTML = "";
 
-        let productInBasket=basket.find(item=>item.id===parseInt(key))
-        //console.log(productInBasket.name+val);
-        document.getElementById("litest").innerHTML += 
+    const counts = {}
+    basket.forEach((product, index) => {
+        counts[product.id] = (counts[product.id] || 0) + 1;
+    });
+
+
+    Object.entries(counts).forEach(([key, val]) => {
+
+        let productInBasket = basket.find(item => item.id === parseInt(key))
+
+
+        document.getElementById("product").innerHTML +=
             `
             <li class="list-group-item d-flex flex-row justify-content-between ">
                 <div d-flex flex-row">
@@ -48,103 +40,30 @@ function basketList(){
                 <button class="btn btn-danger" style="padding: 0px 5px;" onClick="deleteProduct(${productInBasket.id})"><i class="bi bi-x"></i></button>
                 </div>
             </li>
-        `
+            `
     });
+
+    totalPrice()
     
 
-    
 
-
-    //console.log(counts);
-    // basket.forEach(element => {
-
-        
-    //     console.log(element.id===1);
-    //     //console.log(counts);
-    // });
-
-    // console.log("id: "+Object.keys(counts));
-    // console.log("adet: "+Object.values(counts));
-    // console.log("****");
-    // Object.entries(counts).forEach(([key,val]) => {
-    //     //console.log("key:"+key+" "+"val: "+val);
-        
-
-    // });
-
-//     basket.forEach((product,index) => {
-//     document.getElementById("litest").innerHTML += 
-//     `
-//     <li class="list-group-item d-flex flex-row justify-content-between ">
-//         <div d-flex flex-row">
-//         <span class="my-auto lh-1" style="font-size:small; white-space: nowrap;width: 50px;overflow: hidden;text-overflow: ellipsis; ">${product.name}</span>
-//         </div> 
-//         <div>
-//         <span class="fw-bold">${product.price["2020"]}</span> 
-//         </div>
-//         <div class="my-auto d-flex flex-row">
-//         <span class="fw-bold me-2 my-auto " style="font-size:0.7rem;">1 Adet</span> 
-//         <button class="btn btn-danger" style="padding: 0px 5px;" onClick="deleteProduct(${index})"><i class="bi bi-x"></i></button>
-//         </div>
-//     </li>
-// `})
-    
-    
-
-    let totalPrice=0
-    basket.forEach(product => {
-        totalPrice+=product.price[year]
-    });
-    totalPrice > 0 ? document.getElementById("totalPrice").innerHTML="Toplam: "+totalPrice.toFixed(2)+" TL" : document.getElementById("totalPrice").innerHTML=""
-    
 }
 
-function deleteProduct(productId){
-    //basket = basket.filter((item,itemIndex) => itemIndex !== index)
-    let delItemIndex=0
-    let delItem=basket.find((item,index)=>{
-        item.id===productId ? delItemIndex=index : false
-                
+function totalPrice(){
+    let totalPrice = 0
+    basket.forEach(product => {
+        totalPrice += product.price[year]
+    });
+    totalPrice > 0 ? document.getElementById("totalPrice").innerHTML = "Toplam: " + totalPrice.toFixed(2) + " TL" : document.getElementById("totalPrice").innerHTML = ""
+}
+
+function deleteProduct(productId) {
+    
+    let delItemIndex = 0
+    let delItem = basket.find((item, index) => {
+        item.id === productId ? delItemIndex = index : false
+
     })
-    console.log(delItemIndex);
-    basket = basket.filter((item,itemIndex) => itemIndex !== delItemIndex)
+    basket = basket.filter((item, itemIndex) => itemIndex !== delItemIndex)
     basketList()
 }
-
-{/* <li class="list-group-item d-flex flex-row justify-content-between">
-                          <span class="my-auto">An item</span> 
-                          <div class="my-auto">
-                              <span class="fw-bold me-2">3 Adet</span> 
-                              <button class="btn btn-danger">x</button>
-                            </div>
-</li>
-
-
-
-<div class="card mt-3 ms-auto" style="width: 18rem;">
-    <div class="card-body">
-    <h5 class="card-title">${product.name}</h5>
-    <p class="card-text">${product.brand}</p>
-    <p class="card-text">${product.price} TL</p>
-    <button class="btn btn-danger" onClick="deleteProduct(${index})">Sil</button>
-    </div>
-    </div> */}
-
-//sdads
-
-// basket.forEach((product,index) => {
-//     document.getElementById("litest").innerHTML += 
-// `
-// <li class="list-group-item d-flex flex-row justify-content-between ">
-//     <div d-flex flex-row">
-//     <span class="my-auto lh-1" style="font-size:small; white-space: nowrap;width: 50px;overflow: hidden;text-overflow: ellipsis; ">${product.name}</span>
-//     </div> 
-//     <div>
-//     <span class="fw-bold">${product.price["2020"]}</span> 
-//     </div>
-//     <div class="my-auto d-flex flex-row">
-//     <span class="fw-bold me-2 my-auto " style="font-size:0.7rem;">1 Adet</span> 
-//     <button class="btn btn-danger" style="padding: 0px 5px;" onClick="deleteProduct(${index})"><i class="bi bi-x"></i></button>
-//     </div>
-// </li>
-// `})
--- products.js
+++ products.js
@@ -1,42 +1,20 @@
-// //first load display
-// let data = fetch("./products.json").then((response) => response.json()).then((json) => json.forEach(product => {
-//   document.getElementById("test3").innerHTML += 
-//   `
-//   <div class="col-md-6 col-lg-4 d-flex justify-content-center">
-//       <div class="card mt-3" style="width: 18rem; border:none;">
-//       <img style="width: 200px;height: 200px;" src="${product.img}" class="card-img-top mx-auto mt-2" alt="${product.brand}">
-//       <div class="card-body text-center">
-//       <h6 class="card-title">${product.name}</h6>
-//       <p class="card-text">${product.brand}</p>
-//       <p class="card-text fw-bold">${product.price["2021"]}TL</p>
-//       <div class="d-flex justify-content-center">
-//       <button class="btn btn-primary" onClick="addProduct(${product.id})">Sepete Ekle</button>
-//       </div>
-      
-//       </div>
-//       </div>
-//   </div>
-  
-// `
-// }))
-
 
 //data change as year
 let year=""
 function getData(handleYear="2021"){
   year=handleYear
-  console.log(year);
+
   basket=[]
-  document.getElementById("litest").innerHTML="";
+  document.getElementById("product").innerHTML="";
   document.getElementById("totalPrice").innerHTML=""
-  document.getElementById("test3").innerHTML="";
+  document.getElementById("products").innerHTML="";
+  document.getElementById("emptyAlert").style.visibility="visible"
+  basketList()
 
   fetch("./products.json").then((response) => response.json()).then((data) => data.forEach(product => {
-    //console.log(product);
-    //console.log("fetch içi year:"+year);
-    document.getElementById("test3").innerHTML += 
+    document.getElementById("products").innerHTML += 
     `
-    <div class="col-md-6 col-lg-4 d-flex justify-content-center">
+    <div class="col-md-6 col-lg-6 d-flex justify-content-center">
         <div class="card mt-3" style="width: 18rem; border:none;">
         <img style="width: 200px;height: 200px;" src="${product.img}" class="card-img-top mx-auto mt-2" alt="${product.brand}">
         <div class="card-body text-center">
@@ -44,7 +22,7 @@ function getData(handleYear="2021"){
         <p class="card-text">${product.brand}</p>
         <p class="card-text fw-bold">${product.price[handleYear]}TL</p>
         <div class="d-flex justify-content-center">
-        <button class="btn btn-primary" onClick="addProduct(${product.id},${handleYear})">Sepete Ekle</button>
+        <button class="btn btn-primary" onClick="addProduct(${product.id})">Sepete Ekle</button>
         </div>
         
         </div>
